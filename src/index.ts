c9bfe3ef7e659d9c20a9ae6719e2376bb8d82a19
Federico Moretti <hello@federicomoretti.it>
2025-08-23 17:43:12 +0200

    fix: update decoder function

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,33 @@
 #!/usr/bin/env node
 
+import chalk from 'chalk';
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
 import { run as decoder } from './lib/utils/decode.js';
 import { run as encoder } from './lib/utils/encode.js';
-import chalk from 'chalk';
 
 const init: any = async (): Promise<void> => {
   console.clear();
   try {
-    await yargs()
-            .command('decode', 'JWT Decoder', decoder)
-            .command('encode', 'JWT Encoder', encoder)
+    await yargs(hideBin(process.argv))
+            .command(
+              'decode',
+              'JWT Decoder',
+              (yargs) => {
+                // TODO: @fedtti
+              },
+              (argv) => {
+                decoder(argv.token);
+              }
+            )
+            .command(
+              'encode',
+              'JWT Encoder',
+              encoder
+            )
             .version('JWT Debugger CLI 1.0.0')
             .help()
-            .parse(hideBin(process.argv));
+            .parse();
   } catch (error) {
     console.error(chalk.red.bold(`\n\r${error}`));
     process.exit(1);
--- src/lib/utils/decode.ts
+++ src/lib/utils/decode.ts
@@ -1,7 +1,16 @@
-export const run: any = async (): Promise<void> => {
+import { input, select } from '@inquirer/prompts';
+import chalk from 'chalk';
+import jwt from 'jsonwebtoken';
+
+export const run: any = async (token?: string): Promise<void> => {
   try {
-
+    if (!token) {
+      token = await input({
+        message: chalk.blue('JSON Web Token (JWT): ')
+      });
+    }
   } catch (error) {
-
+    console.error(chalk.red.bold(`\n\r${error}`));
+    process.exit(1);
   }
 };
